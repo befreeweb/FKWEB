60afd93abb3dd7bbe067da116ff6b3f751568544
befreeweb <befreeweb7@gmail.com>
2025-09-07 08:11:53 +0700

    Convert to HTML/CSS/JS page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>This project has been converted to pure HTML/CSS/JS. Please use index.html as the main file.</p>
+    </div>
+  );
+}
+
+export default App;
